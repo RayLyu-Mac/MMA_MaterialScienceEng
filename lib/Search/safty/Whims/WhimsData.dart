18a7613a90afced64bb4daa7f940f3145e68be97
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2021-06-01 11:43:31 -0400

    extinguisher

--- lib/Search/safty/Whims/WhimsData.dart
+++ lib/Search/safty/Whims/WhimsData.dart
@@ -85,6 +85,23 @@ class testdetailData {
       ..add(testdetailData(
           picLeft: 10,
           titleFontsize: 13,
+          title:
+              "Division 2 - Poisonous and Infectious Material Other Toxic Effect)",
+          content:
+              "May cause death or permanent injury following repeated or long term exposure May irritate eyes, skin and breathing and may lead to  long term lung problems and skin sensitivity May cause liver or kidney damage, cancer, birth defects  or sterility Avoid inhalation, skin and eye contact Wear recommended personal protective equipment Wash hands after use",
+          addOnImg:
+              "https://github.com/RayLyu-Mac/MMA_MaterialScienceEng/blob/main/assest/Safty/whims/tip.png?raw=true"))
+      ..add(testdetailData(
+          picLeft: 10,
+          titleFontsize: 13,
+          title: "Class F Dangerously reactive material )",
+          content:
+              "May be unstable, reacting dangerously to jarring, compression, heat or light exposure. May burn, explode or produce dangerous gases when mixed with incompatible materials \nStore away from heat sources Avoid shock and friction Wear the recommended personal protective equipment",
+          addOnImg:
+              "https://github.com/RayLyu-Mac/MMA_MaterialScienceEng/blob/main/assest/Safty/whims/dang.png?raw=true"))
+      ..add(testdetailData(
+          picLeft: 10,
+          titleFontsize: 13,
           title: "Exploding Bomb \n(Explosion or reactivity hazards)",
           content:
               "The symbol of an object exploding is used to clearly signify that the material is explosive and risks combusting when handled improperly. These materials should be handled only by professionals under safe conditions. Proper storage of these materials is paramount as some may be sensitive to heat or light and pose a massive risk for serious injury or death.",
