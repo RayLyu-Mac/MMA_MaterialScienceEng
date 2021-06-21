f76e9b52cda8d506964756950e5bf03e9d707540
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2021-06-21 19:53:44 -0400

    Update STDMain.dart

--- lib/Search/tools/NormalDistribution/STDMain.dart
+++ lib/Search/tools/NormalDistribution/STDMain.dart
@@ -31,69 +31,123 @@ class _STDCheckState extends State<STDCheck> {
           title: Text("STD value check"),
           backgroundColor: Colors.black,
         ),
-        body: Center(
-          child: Container(
-            height: _screenH / 1.5,
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.8),
-              border: Border.all(width: 5, color: Colors.grey[200]),
-            ),
+        body: SingleChildScrollView(
+          child: Center(
             child: Column(
               children: [
                 SizedBox(
                   height: _screenH / 25,
                 ),
                 Container(
+                  width: _screenWidth / 1.1,
+                  height: _screenH / 2,
                   padding: EdgeInsets.fromLTRB(_screenWidth / 10, _screenH / 30,
                       _screenWidth / 10, _screenH / 30),
-                  width: _screenWidth / 1.1,
-                  height: _screenH / 10,
-                  child: TextField(
-                    controller: before,
-                    decoration: InputDecoration(hintText: "Z value:"),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.8),
+                    border: Border.all(width: 7, color: Colors.grey[200]),
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: _screenH / 45,
+                      ),
+                      Text(
+                        "Convert Z value to Standard Normal Distribution",
+                        style: TextStyle(
+                            fontSize: _screenH / 35,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
                   ),
                 ),
                 SizedBox(
-                  height: _screenH / 45,
-                ),
-                RaisedButton.icon(
-                    padding: EdgeInsets.fromLTRB(_screenWidth / 20,
-                        _screenH / 50, _screenWidth / 20, _screenH / 50),
-                    shape: Border.all(width: 5, color: Colors.grey[700]),
-                    onPressed: () {
-                      setState(() {
-                        double input = double.parse(before.text);
-                        if (stdNum[input
-                                .toString()
-                                .substring(0, input.toString().length - 1)] ==
-                            null) {
-                          output = "Please correct your input";
-                        } else {
-                          output = stdNum[input.toString().substring(
-                                          0, input.toString().length - 1)]
-                                      [input.toString().length]
-                                  .toString() ??
-                              "Please correct your input";
-                        }
-                      });
-                    },
-                    icon: Icon(Icons.calculate_rounded),
-                    label: Text(
-                      "Find STD value",
-                      style: TextStyle(
-                          fontSize: _screenH / 45, fontWeight: FontWeight.bold),
-                    )),
-                SizedBox(
-                  height: _screenH / 45,
+                  height: _screenH / 25,
                 ),
                 Container(
-                  child: Text(
-                    "The Probabalilty Density with Z score " +
-                            before.text.toString() ??
-                        "--" + " is " + output ??
-                        "--",
-                    style: TextStyle(
-                        fontSize: _screenH / 40, fontWeight: FontWeight.bold),
+                  width: _screenWidth / 1.1,
+                  height: _screenH / 1.3,
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.8),
+                    border: Border.all(width: 7, color: Colors.grey[200]),
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: _screenH / 45,
+                      ),
+                      Text(
+                        "Find the probablity density for score Z",
+                        style: TextStyle(
+                            fontSize: _screenH / 35,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(
+                        height: _screenH / 25,
+                      ),
+                      Image(
+                          image: NetworkImage(
+                              "https://github.com/RayLyu-Mac/MMA_MaterialScienceEng/blob/main/assest/STD.png?raw=true")),
+                      SizedBox(
+                        height: _screenH / 25,
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(_screenWidth / 10,
+                            _screenH / 30, _screenWidth / 10, _screenH / 30),
+                        width: _screenWidth / 1.1,
+                        height: _screenH / 10,
+                        child: TextField(
+                          controller: before,
+                          decoration: InputDecoration(hintText: "Z value:"),
+                        ),
+                      ),
+                      SizedBox(
+                        height: _screenH / 45,
+                      ),
+                      RaisedButton.icon(
+                          padding: EdgeInsets.fromLTRB(_screenWidth / 20,
+                              _screenH / 50, _screenWidth / 20, _screenH / 50),
+                          shape: Border.all(width: 5, color: Colors.grey[700]),
+                          onPressed: () {
+                            setState(() {
+                              double input = double.parse(before.text);
+                              if (stdNum[input.toString().substring(
+                                      0, input.toString().length - 1)] ==
+                                  null) {
+                                output = "Please correct your input";
+                              } else {
+                                output = stdNum[input.toString().substring(
+                                                0, input.toString().length - 1)]
+                                            [input.toString().length]
+                                        .toString() ??
+                                    "Please correct your input";
+                              }
+                            });
+                          },
+                          icon: Icon(Icons.calculate_rounded),
+                          label: Text(
+                            "Find STD value",
+                            style: TextStyle(
+                                fontSize: _screenH / 45,
+                                fontWeight: FontWeight.bold),
+                          )),
+                      SizedBox(
+                        height: _screenH / 45,
+                      ),
+                      Container(
+                        child: Text(
+                          "The Probabalilty Density with Z score " +
+                                  before.text.toString() ??
+                              "--" + " is " + output ??
+                              "--",
+                          style: TextStyle(
+                              fontSize: _screenH / 40,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ],
                   ),
                 )
               ],
