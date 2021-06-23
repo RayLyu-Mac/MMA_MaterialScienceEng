3d46bbd363f4848797a92bfb5085d0b882244dec
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2021-06-23 09:15:26 -0400

    reference

---
+++ lib/relatedInfo/referenceData.dart
@@ -0,0 +1,16 @@
+Map<String, List<String>> refernce = {
+  "Stats Toolo": [
+    "https://www.youtube.com/watch?v=2tuBREK_mgE&t=90s",
+    "https://www.thoughtco.com/combinations-vs-permutations-3126548"
+  ],
+  "Phase Diagram": [
+    "https://zh.wikipedia.org/wiki/%E9%93%81%E7%A2%B3%E5%90%88%E9%87%91%E7%9B%B8%E5%9B%BE",
+    "https://www.researchgate.net/figure/The-phase-diagram-of-Cu-Zn-alloy_fig7_287317290",
+    "https://commons.wikimedia.org/wiki/File:Binary_phase_diagram_of_AlCu_(temperature).JPG",
+    "https://www.southampton.ac.uk/~pasr1/al-cu.htm"
+  ],
+  "Fire Safety": [
+    "https://bidfiresafety.com/fire-extinguisher-types/",
+    "https://fhs.mcmaster.ca/safetyoffice/documents/FireFHSinfosheetJanuary2020V2.pdf"
+  ],
+};
