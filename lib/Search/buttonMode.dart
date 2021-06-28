c7ff02c920416e30c99e2043b81a310805e96d60
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2021-06-28 11:36:51 -0400

    a

--- lib/Search/buttonMode.dart
+++ lib/Search/buttonMode.dart
@@ -45,11 +45,11 @@ class _IntroButtonModeState extends State<IntroButtonMode> {
   @override
   Widget build(BuildContext context) {
     Map<String, List> position = {
-      "a": [_screenWidth / 19, _screenH / 50, _screenH / 2.8],
-      "b": [_screenWidth / 1.9, _screenH / 50, _screenH / 5],
-      "c": [_screenWidth / 1.9, _screenH / 4.2, _screenH / 2.8],
-      "d": [_screenWidth / 19, _screenH / 2.5, _screenH / 5],
-      "e": [_screenWidth / 1.9, _screenH / 1.6, _screenH / 5],
+      "a": [_screenWidth / 19, _screenH / 3.8, _screenH / 2.4],
+      "b": [_screenWidth / 1.9, _screenH / 50, _screenH / 4.4],
+      "c": [_screenWidth / 1.9, _screenH / 3.8, _screenH / 2.4],
+      "d": [_screenWidth / 19, _screenH / 1.42, _screenH / 4.4],
+      "e": [_screenWidth / 1.9, _screenH / 1.42, _screenH / 4.4],
       "f": [_screenWidth / 19, _screenH / 2.6, _screenH / 5]
     };
     return widget.str != false
@@ -81,50 +81,57 @@ class _IntroButtonModeState extends State<IntroButtonMode> {
                       color: Colors.black,
                       fontSize: widget.fontsize ?? _screenH / 35,
                       fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
                 ),
                 decoration: BoxDecoration(
                     color: Colors.white.withOpacity(0.8),
                     borderRadius: BorderRadius.circular(25),
                     border: Border.all(width: 5, color: Colors.grey[600]),
                     image: DecorationImage(
+                        colorFilter: new ColorFilter.mode(
+                            Colors.black.withOpacity(0.4), BlendMode.dstATop),
                         image: NetworkImage(widget.backImg),
                         fit: BoxFit.cover)),
               ),
             ))
-        : InkWell(
-            hoverColor: Colors.white.withOpacity(0.4),
-            splashColor: Colors.white.withOpacity(0.2),
-            onTap: () {
-              Navigator.push(
-                  context,
-                  PageTransition(
-                    duration: const Duration(milliseconds: 500),
-                    child: widget.pageTo,
-                    type: PageTransitionType.leftToRight,
-                  ));
-            },
-            child: Container(
-              alignment: Alignment.center,
-              constraints: BoxConstraints.expand(
-                  width: _screenWidth / 2.7, height: _screenH / 2.8),
-              child: Text(
-                widget.title ?? "_",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: widget.fontsize != null
-                        ? widget.fontsize
-                        : _screenH / 35,
-                    fontWeight: FontWeight.bold),
-              ),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                        offset: const Offset(4, 4), color: Colors.blue[100])
-                  ],
-                  image: DecorationImage(
-                      image: NetworkImage(widget.backImg), fit: BoxFit.cover)),
-            ),
+        : Container(
+            alignment: Alignment.center,
+            constraints: BoxConstraints.expand(
+                width: _screenWidth / 2.7, height: _screenH / 2.8),
+            decoration: BoxDecoration(
+                color: const Color(0xff7c94b6),
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(offset: const Offset(4, 4), color: Colors.blue[100])
+                ],
+                image: DecorationImage(
+                    colorFilter: new ColorFilter.mode(
+                        Colors.black.withOpacity(0.4), BlendMode.dstATop),
+                    image: NetworkImage(widget.backImg),
+                    fit: BoxFit.cover)),
+            child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      PageTransition(
+                        duration: const Duration(milliseconds: 500),
+                        child: widget.pageTo,
+                        type: PageTransitionType.leftToRight,
+                      ));
+                },
+                child: Container(
+                  decoration: BoxDecoration(color: Colors.green),
+                  child: Text(
+                    widget.title ?? "_",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: widget.fontsize != null
+                          ? widget.fontsize
+                          : _screenH / 35,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )),
           );
   }
 }
--- lib/Search/tools/toolsMain.dart
+++ lib/Search/tools/toolsMain.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mma_mse/Search/buttonMode.dart';
 
 import 'tooData.dart';
@@ -32,13 +33,24 @@ class _toolMainState extends State<toolMain> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.grey[200],
-        appBar: AppBar(
-          title: Text("Tools Main"),
-          backgroundColor: Colors.black,
-        ),
         body: SafeArea(
           child: Stack(
             children: [
+              Positioned(
+                  top: _screenH / 20,
+                  left: _screenWidth / 15,
+                  child: Column(
+                    children: [
+                      Text(
+                        "Material \nTool Boxs",
+                        style: TextStyle(
+                            fontSize: _screenH / 22,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                      Icon(FontAwesomeIcons.toolbox)
+                    ],
+                  )),
               for (var i = 0; i < toolDataList.length; i++)
                 IntroButtonMode(
                   str: true,
