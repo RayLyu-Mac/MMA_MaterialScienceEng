00bcfa074d8d73f9ac0500e113ee6b63bc96f408
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2021-09-10 21:22:53 -0400

    a

--- lib/Instruction/MountPress/EcoPress2000/EcoPressData.dart
+++ lib/Instruction/MountPress/EcoPress2000/EcoPressData.dart
@@ -6,16 +6,24 @@ class video {
   String subtitle;
   YoutubePlayerController videoController;
   Color materialcolor;
+  String type;
 
   video({
     this.title,
     this.subtitle,
+    this.type,
     this.materialcolor,
     this.videoController,
   });
   List<video> videoList() {
     List videoData = List<video>();
     return videoData
+      ..add(video(
+        type: "tit",
+        title: "Preparation",
+        subtitle: "Ready the mount press machine",
+        materialcolor: Colors.amber[600],
+      ))
       ..add(video(
           title: "Preparation before start",
           subtitle: "Set the program and clean the chamber",
@@ -27,11 +35,17 @@ class video {
               mute: false,
             ),
           )))
+      ..add(video(
+        type: "tit",
+        title: "Operation",
+        subtitle: "How to operate the mount press",
+        materialcolor: Colors.green[600],
+      ))
       ..add(
         video(
             title: "Start the pressing",
             subtitle: "Press the button and press will automatically start",
-            materialcolor: Colors.red[400],
+            materialcolor: Colors.green[300],
             videoController: YoutubePlayerController(
               initialVideoId: 'iwIrttSuW5k',
               flags: YoutubePlayerFlags(
@@ -44,7 +58,7 @@ class video {
         video(
             title: "Making a Metallurgical Mount",
             subtitle: "High Temperature 175 degree C will be applied",
-            materialcolor: Colors.pink[400],
+            materialcolor: Colors.green[300],
             videoController: YoutubePlayerController(
               initialVideoId: '_RAZC-_Zpdk',
               flags: YoutubePlayerFlags(
@@ -56,7 +70,7 @@ class video {
       ..add(video(
           title: "Remove sample from the mold",
           subtitle: "Follow th emessage display in the screen",
-          materialcolor: Colors.green[400],
+          materialcolor: Colors.green[300],
           videoController: YoutubePlayerController(
             initialVideoId: 'qnVvgwEi_z0',
             flags: YoutubePlayerFlags(
