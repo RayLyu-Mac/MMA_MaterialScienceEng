0800458d6ddd6ecb9736d3120595e4f1006558fa
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2021-09-10 21:43:16 -0400

    a

--- lib/Instruction/MicroHardnessTester/MicroHardnessData.dart
+++ lib/Instruction/MicroHardnessTester/MicroHardnessData.dart
@@ -6,16 +6,24 @@ class video {
   String subtitle;
   YoutubePlayerController videoController;
   Color materialcolor;
+  String type;
 
   video({
     this.title,
     this.subtitle,
+    this.type,
     this.materialcolor,
     this.videoController,
   });
   List<video> videoList() {
     List videoData = List<video>();
     return videoData
+      ..add(video(
+        type: "tit",
+        title: "Preparation",
+        subtitle: "Place the sample and start the tester",
+        materialcolor: Colors.amber[600],
+      ))
       ..add(video(
           title: "Load Sample",
           subtitle:
@@ -32,7 +40,7 @@ class video {
         video(
             title: "Adjust the Load of the microscope",
             subtitle: "Changing the load based on the material that is testing",
-            materialcolor: Colors.amber[400],
+            materialcolor: Colors.amber[300],
             videoController: YoutubePlayerController(
               initialVideoId: 'aN9HMvILYYU',
               flags: YoutubePlayerFlags(
@@ -45,7 +53,7 @@ class video {
         video(
             title: "Focusing on the sample",
             subtitle: "Start with 100X and use the joystick to focus",
-            materialcolor: Colors.pink[400],
+            materialcolor: Colors.amber[300],
             videoController: YoutubePlayerController(
               initialVideoId: 'G9X9vON7dFk',
               flags: YoutubePlayerFlags(
@@ -55,10 +63,16 @@ class video {
             )),
       )
       ..add(video(
+        type: "tit",
+        title: "Operation",
+        subtitle: "SOPs for microhardness tester",
+        materialcolor: Colors.green[600],
+      ))
+      ..add(video(
           title: "Single Point Indent",
           subtitle:
               "Find a suitable surface and use joystick to move the sample",
-          materialcolor: Colors.red[400],
+          materialcolor: Colors.green[300],
           videoController: YoutubePlayerController(
             initialVideoId: 'T8a0dO9LqOQ',
             flags: YoutubePlayerFlags(
@@ -67,9 +81,15 @@ class video {
             ),
           )))
       ..add(video(
+        type: "tit",
+        title: "Template",
+        subtitle: "How to use different mode of template",
+        materialcolor: Colors.limeAccent[700],
+      ))
+      ..add(video(
           title: "Field Template",
           subtitle: "Pattern needed (multiple indent)",
-          materialcolor: Colors.green[400],
+          materialcolor: Colors.limeAccent[200],
           videoController: YoutubePlayerController(
             initialVideoId: 'hpK5HnruF_s',
             flags: YoutubePlayerFlags(
@@ -81,7 +101,7 @@ class video {
           title: "Linear Template",
           subtitle:
               "Linear Template is done when a straight line is required (preforming a Case-Hardened Sample).",
-          materialcolor: Colors.green,
+          materialcolor: Colors.limeAccent[200],
           videoController: YoutubePlayerController(
             initialVideoId: 'J6HhFkc3Auo',
             flags: YoutubePlayerFlags(
@@ -93,7 +113,7 @@ class video {
           title: "Field Point Template",
           subtitle:
               "Field Point is use when random points are required around the sample.",
-          materialcolor: Colors.green,
+          materialcolor: Colors.limeAccent[200],
           videoController: YoutubePlayerController(
             initialVideoId: 'kmIsfgi7E_E',
             flags: YoutubePlayerFlags(
@@ -102,9 +122,15 @@ class video {
             ),
           )))
       ..add(video(
+        type: "tit",
+        title: "Finish and Clean Up",
+        subtitle: "Clean and organize after experiment finished",
+        materialcolor: Colors.blue[600],
+      ))
+      ..add(video(
           title: "Finish the Test and Clean Up",
           subtitle: "Shut down the machine and unload the sample",
-          materialcolor: Colors.green,
+          materialcolor: Colors.blue[300],
           videoController: YoutubePlayerController(
             initialVideoId: '4ggDqgPr3SM',
             flags: YoutubePlayerFlags(
--- lib/Instruction/MicroHardnessTester/MicroHardnessMain.dart
+++ lib/Instruction/MicroHardnessTester/MicroHardnessMain.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
 import 'MicroHardnessData.dart';
 import 'package:flutter/services.dart';
-import 'package:mma_mse/customTileScroll.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:mma_mse/Instruction/InstructionMode.dart';
 
 class MHTinstruction extends StatefulWidget {
   final video videoType;
@@ -15,18 +15,14 @@ class MHTinstruction extends StatefulWidget {
 
 class _MHTinstructionState extends State<MHTinstruction> {
   List _instructionList = List<video>();
-  double _screenWidth;
-  double _screenH;
+
   final ScrollController controller = ScrollController();
-  List titles = [];
 
   @override
   void initState() {
     super.initState();
     _instructionList = video().videoList();
-    for (var j = 0; j < _instructionList.length; j++) {
-      titles.add(_instructionList[j].title);
-    }
+
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
@@ -34,96 +30,10 @@ class _MHTinstructionState extends State<MHTinstruction> {
   }
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _screenWidth = MediaQuery.of(context).size.width;
-    _screenH = MediaQuery.of(context).size.height;
-  }
-
-  @override
   Widget build(BuildContext context) {
-    Drawer microhardnessInstr() => Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(
-                child: Text("Instruction For MicroHardness Tester"),
-              ),
-              for (var i = 0; i < titles.length; i++)
-                ScrollcustomListTile(
-                    name: titles[i],
-                    pageTo: i - 1,
-                    fonts: 13,
-                    controller: controller),
-            ],
-          ),
-        );
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          actions: [
-            IconButton(
-                padding: EdgeInsets.fromLTRB(10, 3, 19, 3),
-                iconSize: 32,
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: Icon(FontAwesomeIcons.timesCircle))
-          ],
-          title: Text(
-            "Micro-Hardness Tester instruction",
-            style:
-                TextStyle(fontSize: _screenH / 35, fontWeight: FontWeight.bold),
-          ),
-        ),
-        drawer: microhardnessInstr(),
-        body: SafeArea(
-          child: ListView.builder(
-            itemExtent: 330,
-            controller: controller,
-            itemCount: _instructionList.length,
-            itemBuilder: (BuildContext context, int index) {
-              return GestureDetector(
-                child: Card(
-                  color: _instructionList[index].materialcolor,
-                  margin: EdgeInsets.fromLTRB(20, 16, 20, 8),
-                  elevation: 5,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0)),
-                  clipBehavior: Clip.antiAlias,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                          top: 250,
-                          left: 5,
-                          child: Text(
-                            _instructionList[index].title,
-                            style: TextStyle(
-                                fontSize: _screenH / 47,
-                                fontWeight: FontWeight.bold),
-                          )),
-                      Positioned(
-                          top: 270,
-                          left: 5,
-                          child: Text(
-                            _instructionList[index].subtitle,
-                            style: TextStyle(fontSize: _screenH / 58),
-                          )),
-                      Positioned(
-                          top: 0,
-                          child: Container(
-                            height: 245,
-                            child: YoutubePlayer(
-                              controller:
-                                  _instructionList[index].videoController,
-                              liveUIColor: Colors.amber,
-                            ),
-                          ))
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),
-        ));
+    return InstructionMode(
+        instructionList: _instructionList,
+        name: "Micro-Hardness Tester",
+        controller: controller);
   }
 }
