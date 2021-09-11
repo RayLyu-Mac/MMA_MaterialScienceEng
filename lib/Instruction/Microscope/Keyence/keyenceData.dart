f4e937b271bf7493adedfe00c41f3344753de7af
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2021-09-10 21:32:17 -0400

    a

--- lib/Instruction/Microscope/Keyence/keyenceData.dart
+++ lib/Instruction/Microscope/Keyence/keyenceData.dart
@@ -6,16 +6,24 @@ class video {
   String subtitle;
   YoutubePlayerController videoController;
   Color materialcolor;
+  String type;
 
   video({
     this.title,
     this.subtitle,
+    this.type,
     this.materialcolor,
     this.videoController,
   });
   List<video> videoList() {
     List videoData = List<video>();
     return videoData
+      ..add(video(
+        type: "tit",
+        title: "Preparation",
+        subtitle: "Ready the Microscope",
+        materialcolor: Colors.amber[600],
+      ))
       ..add(video(
           title: "Start the machine",
           subtitle: "Press the power button",
@@ -31,7 +39,7 @@ class video {
         video(
             title: "Placing the sample into the stage",
             subtitle: "Prepare the sample for the test",
-            materialcolor: Colors.amber[400],
+            materialcolor: Colors.amber[300],
             videoController: YoutubePlayerController(
               initialVideoId: '3ms1xMMMQjc',
               flags: YoutubePlayerFlags(
@@ -45,7 +53,7 @@ class video {
             title: "Adjust the light for the microscope",
             subtitle:
                 "ID brightfield, polarize and dark field light, adjust them based on the case",
-            materialcolor: Colors.amber[600],
+            materialcolor: Colors.amber[300],
             videoController: YoutubePlayerController(
               initialVideoId: 'V8abiMLr2cY',
               flags: YoutubePlayerFlags(
@@ -54,11 +62,17 @@ class video {
               ),
             )),
       )
+      ..add(video(
+        type: "tit",
+        title: "Operation",
+        subtitle: "SOPs for microscope inspection",
+        materialcolor: Colors.green[600],
+      ))
       ..add(
         video(
             title: "Taking simple image",
             subtitle: "Process of taking image and save",
-            materialcolor: Colors.pink[400],
+            materialcolor: Colors.green[300],
             videoController: YoutubePlayerController(
               initialVideoId: '4hs7BH65vLI',
               flags: YoutubePlayerFlags(
@@ -71,7 +85,7 @@ class video {
           title: "Correcting a sample which is not flat",
           subtitle:
               "Find the lowest point of the sample using either the Fine or Coarse Focusing.",
-          materialcolor: Colors.pink[600],
+          materialcolor: Colors.green[300],
           videoController: YoutubePlayerController(
             initialVideoId: 'cnUS2RppP7U',
             flags: YoutubePlayerFlags(
@@ -82,7 +96,7 @@ class video {
       ..add(video(
           title: "Large Image Scan",
           subtitle: "Chose 2D or 3D, 3D is required when sample is not flat",
-          materialcolor: Colors.redAccent,
+          materialcolor: Colors.green[300],
           videoController: YoutubePlayerController(
             initialVideoId: 'bDzxWiM7kwY',
             flags: YoutubePlayerFlags(
@@ -91,9 +105,15 @@ class video {
             ),
           )))
       ..add(video(
+        type: "tit",
+        title: "Post-processing",
+        subtitle: "Get a better image for analysing",
+        materialcolor: Colors.purpleAccent[700],
+      ))
+      ..add(video(
           title: "Enhancing the resolution",
           subtitle: "Chose 2D or 3D, 3D is required when sample is not flat",
-          materialcolor: Colors.blue[400],
+          materialcolor: Colors.purpleAccent,
           videoController: YoutubePlayerController(
             initialVideoId: 'SvwAnZuChzA',
             flags: YoutubePlayerFlags(
@@ -104,7 +124,7 @@ class video {
       ..add(video(
           title: "Selecting the best image",
           subtitle: "Choose a best and clear images",
-          materialcolor: Colors.blue,
+          materialcolor: Colors.purpleAccent,
           videoController: YoutubePlayerController(
             initialVideoId: 'IzNETDmhF9I',
             flags: YoutubePlayerFlags(
@@ -115,7 +135,7 @@ class video {
       ..add(video(
           title: "Glare removal",
           subtitle: "Chose 2D or 3D, 3D is required when sample is not flat",
-          materialcolor: Colors.blue[600],
+          materialcolor: Colors.purpleAccent,
           videoController: YoutubePlayerController(
             initialVideoId: '9TYFOn23E_8',
             flags: YoutubePlayerFlags(
@@ -124,9 +144,15 @@ class video {
             ),
           )))
       ..add(video(
+        type: "tit",
+        title: "Finish and Clean Up",
+        subtitle: "Clean and organize after experiment finished",
+        materialcolor: Colors.blue[600],
+      ))
+      ..add(video(
           title: "Finish the experiment",
           subtitle: "Save the image the shut the microscope",
-          materialcolor: Colors.green,
+          materialcolor: Colors.blue[300],
           videoController: YoutubePlayerController(
             initialVideoId: '6_7OJJNYE_M',
             flags: YoutubePlayerFlags(
--- lib/Instruction/Microscope/Keyence/keyenceMain.dart
+++ lib/Instruction/Microscope/Keyence/keyenceMain.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
 import 'keyenceData.dart';
 import 'package:flutter/services.dart';
-import 'package:mma_mse/customTileScroll.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:mma_mse/Instruction/InstructionMode.dart';
 
 class KeyenceMicroInstru extends StatefulWidget {
   final video videoType;
@@ -15,20 +15,8 @@ class KeyenceMicroInstru extends StatefulWidget {
 
 class _KeyenceMicroInstruState extends State<KeyenceMicroInstru> {
   List _instructionList = List<video>();
-  double _screenWidth;
-  double _screenH;
+
   final ScrollController controller = ScrollController();
-  List title = [
-    "Start the machine",
-    "Placing the sample into the stage",
-    "Adjust the light for the microscope",
-    "Taking simple image",
-    "Correcting a sample which is not flat",
-    "Large Image Scan",
-    "Enhancing the resolution",
-    "Glare removal",
-    "Finish the experiment"
-  ];
 
   @override
   void initState() {
@@ -41,94 +29,10 @@ class _KeyenceMicroInstruState extends State<KeyenceMicroInstru> {
   }
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _screenWidth = MediaQuery.of(context).size.width;
-    _screenH = MediaQuery.of(context).size.height;
-  }
-
-  @override
   Widget build(BuildContext context) {
-    Drawer keyenceInstruction() => Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(child: Text("Keyence Microscope")),
-              for (var j = 0; j < title.length; j++)
-                ScrollcustomListTile(
-                    name: title[j],
-                    pageTo: j - 1,
-                    fonts: 13,
-                    controller: controller)
-            ],
-          ),
-        );
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          actions: [
-            IconButton(
-                padding: EdgeInsets.fromLTRB(10, 3, 19, 3),
-                iconSize: 32,
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: Icon(FontAwesomeIcons.timesCircle))
-          ],
-          title: Text(
-            "Keyence MicroScope Instruction",
-            style:
-                TextStyle(fontSize: _screenH / 35, fontWeight: FontWeight.bold),
-          ),
-        ),
-        drawer: keyenceInstruction(),
-        body: SafeArea(
-          child: ListView.builder(
-            controller: controller,
-            itemExtent: 335,
-            itemCount: _instructionList.length,
-            itemBuilder: (BuildContext context, int index) {
-              return GestureDetector(
-                child: Card(
-                  color: _instructionList[index].materialcolor,
-                  margin: EdgeInsets.fromLTRB(24, 16, 24, 8),
-                  elevation: 5,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0)),
-                  clipBehavior: Clip.antiAlias,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                          top: 270,
-                          left: 5,
-                          child: Text(
-                            _instructionList[index].title,
-                            style: TextStyle(
-                                fontSize: _screenH / 47,
-                                fontWeight: FontWeight.bold),
-                          )),
-                      Positioned(
-                          top: 290,
-                          left: 5,
-                          child: Text(
-                            _instructionList[index].subtitle,
-                            style: TextStyle(fontSize: _screenH / 58),
-                          )),
-                      Positioned(
-                          top: 0,
-                          child: Container(
-                            height: 265,
-                            child: YoutubePlayer(
-                              controller:
-                                  _instructionList[index].videoController,
-                              liveUIColor: Colors.amber,
-                            ),
-                          ))
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),
-        ));
+    return InstructionMode(
+        instructionList: _instructionList,
+        name: "Keyence MicroScope",
+        controller: controller);
   }
 }
