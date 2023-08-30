04343434cd7b981e76ee33e26ca7033d21733d43
raylyu <59774755+RayLyu-Mac@users.noreply.github.com>
2023-08-30 12:55:07 -0400

    ios

---
+++ lib/button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class button extends StatelessWidget {
+  final String text;
+  final VoidCallback onClicked;
+
+  const button({required this.text, required this.onClicked, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => ElevatedButton(
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 24),
+        ),
+        onPressed: onClicked,
+      );
+}
